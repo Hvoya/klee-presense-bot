bc603bb272aaf5955c6c7f24d99f7507f2091817
Hvoya (Kirin Danil) <dkirin@yoomoney.ru>
2023-01-25 03:32:15 +0300

    logging middleware

--- src/middleware/logging.ts
+++ src/middleware/logging.ts
@@ -1,9 +1,8 @@
 import { SceneContextMessageUpdate } from "telegraf/typings/stage";
 import { texts } from "../content/texts";
 import { goToMainMenu } from "../helpers/scenes/go-to-main-menu";
-import { Storage } from "../storage";
 
-export const getLoggingMiddleware = (_: Storage) => async (ctx: SceneContextMessageUpdate, next: () => void) => {
+export const getLoggingMiddleware = () => async (ctx: SceneContextMessageUpdate, next: () => void) => {
     try {
         console.log(ctx.message?.from?.username, ctx.message?.text);
 
