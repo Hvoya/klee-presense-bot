739b6ed53a619b5dde9d3dec486684dfa00bab39
Hvoya (Kirin Danil) <dkirin@yoomoney.ru>
2023-02-05 17:03:33 +1000

    gifs update

--- src/bot/scenes/helpers/go-to-main-menu.ts
+++ src/bot/scenes/helpers/go-to-main-menu.ts
@@ -1,7 +1,9 @@
 import { SceneContextMessageUpdate } from 'telegraf/typings/stage'
 import { mainMenu } from '../../content/keyboards'
+import { gifs } from '../../content/media'
 
 export const goToMainMenu = async (ctx: SceneContextMessageUpdate, text: string): Promise<void> => {
   await ctx.scene.leave()
   await ctx.reply(text, mainMenu)
+  await ctx.replyWithAnimation(gifs.backToBoomKlee)
 }
