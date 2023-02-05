e0afc22744ce1ff7b78f08338514df484c168bf6
Hvoya (Kirin Danil) <dkirin@yoomoney.ru>
2023-02-05 15:42:51 +1000

    migrations fix

--- src/db/migrations/003.do.presenses.sql
+++ src/db/migrations/003.do.presenses.sql
@@ -1,8 +1,5 @@
-DROP TYPE IF EXISTS presense_status;
-CREATE TYPE presense_status AS ENUM ('will-come', 'will-not-come', 'unknown');
-
 CREATE TABLE IF NOT EXISTS presenses (
-    status presense_status,
+    status VARCHAR(255),
     date TIMESTAMP WITH TIME ZONE,
     user_id VARCHAR(255),
     PRIMARY KEY (user_id, date),
