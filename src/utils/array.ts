fe9f79c03427c970591690d6f21bd30f1fc9fa27
Hvoya (Kirin Danil) <dkirin@yoomoney.ru>
2023-02-05 16:55:51 +1000

    keyboard fix

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,5 +1,6 @@
 export const chunkArray = <T>(arr: T[], chunkSize: number): T[][] => arr.reduce<T[][]>((all, one, i) => {
   const ch = Math.floor(i / chunkSize)
-  all[ch] = ([] as T[]).concat((all[ch]), one)
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  all[ch] = ([] as T[]).concat((all[ch] || []), one)
   return all
 }, [])
